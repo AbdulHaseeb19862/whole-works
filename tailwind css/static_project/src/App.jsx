3a8af3b1560ced1c2b4a14c4ce1fe18a89cf4ab6
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2025-03-13 00:50:11 +0500

    Create Project setup

---
+++ tailwind css/static_project/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-4xl bg-green-600">Welcome To Tailwind css project</h2>
+    </>
+  );
+}
+
+export default App;
